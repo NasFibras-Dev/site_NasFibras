e4e4821a3c4f221ca2abcdac413555238828135e
NasFibras-Dev <101998063+NasFibras-Dev@users.noreply.github.com>
2022-03-24 15:07:28 -0300

    update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,90 @@
 //Velocimetro Algar Telecom
 
-window.ST_PARAMS = {
-    "subdomain": "algar",
-    "testConfiguration": {
-        "testConfigurationId": 788,
-        "subscriptionId": 376,
-        "subdomain": "algar",
-        "createdDate": "2017-03-13T09:25:30.000Z",
-        "modifiedDate": "2017-03-18T11:07:37.000Z",
-        "allowedServers": 5,
-        "serverNetwork": "customer",
-        "dataProtectionContactId": 0,
-        "resellerContactId": 0,
-        "apiKey": null,
-        "originHost": null,
-        "autoStart": false,
-        "clientIcon": "user",
-        "customCssFile": "",
-        "customerServers": [27262, 11520, 26486, 28443, 29662],
-        "distanceUnits": "mi",
-        "embedUrls": ["velocidade-fla.algartelecom.com.br", "velocidade-spo.algartelecom.com.br", "velocidade-lgs.algartelecom.com.br", "velocidade-ula.algartelecom.com.br", "velocidade-sdr.algartelecom.com.br", "velocidade.algartelecom.com.br"],
-        "hostSelectDisabled": false,
-        "httpsOnly": false,
-        "isActive": true,
-        "isUpgradeDisabled": false,
-        "allowSpeedtestUrls": false,
-        "logo": { "name": "rsz_1logo_algar.png", "cdnFileName": "788-1489860003519.png" },
-        "passwordProtected": false,
-        "themeColors": { "backgroundColor": "#ffffff", "primaryColor": "#0268a8" },
-        "serverIcon": "internet",
-        "speedLabel": "Mbps",
-        "defaultMaxSpeed": "100",
-        "testTypes": ["upload", "download", "jitter", "ping"],
-        "ipServerAssignment": { "allowed": false, "enabled": false },
-        "isPaid": true,
-        "isEUCountry": false
-    },
-    "config": { "rootDomain": "speedtestcustom.com", "logoCdnUrl": "https:\u002F\u002Flogos.speedtestcustom.com\u002Fprod\u002F", "countriesWithVat": ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB"], "premiumSubscriptionBasePrice": 1995, "premiumSubscriptionServerPrice": 995, "auth0": { "domain": "ookla.auth0.com", "clientId": "qZ22rLQ3HmY102QN05DIQGqFkz3AHdFr", "loginRedirectUrl": "https:\u002F\u002Faccount.speedtestcustom.com", "logoutRedirectUrl": "https:\u002F\u002Faccount.speedtestcustom.com\u002Flogin", "forgotPasswordLink": "https:\u002F\u002Faccount.speedtestcustom.com\u002Freset-password", "authorization": { "audience": "https:\u002F\u002Fapi.st-connect.com" }, "management": { "connection": "OOKLA-AUTH" } }, "privacyPolicyUpdated": "2018-05-18" },
-    "testReport": null,
-    "serverList": [{ "url": "http:\u002F\u002Fvelocidade-spo.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -23.5, "lon": -46.6167, "name": "São Paulo", "cc": "BR", "distance": 60, "sponsor": "Algar Telecom", "id": "28443", "host": "velocidade-spo.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-lgs.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -27.8157, "lon": -50.3264, "name": "Lages", "cc": "BR", "distance": 319, "sponsor": "Algar Telecom", "id": "27262", "host": "velocidade-lgs.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-ula.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -18.9, "lon": -48.2833, "name": "Uberlândia", "cc": "BR", "distance": 381, "sponsor": "Algar Telecom", "id": "11520", "host": "velocidade-ula.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-sdr.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -12.9747, "lon": -38.4767, "name": "Salvador", "cc": "BR", "distance": 962, "sponsor": "Algar Telecom", "id": "26486", "host": "velocidade-sdr.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-fla.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -3.7183, "lon": -38.5428, "name": "Fortaleza", "cc": "BR", "distance": 1531, "sponsor": "Algar Telecom", "id": "29662", "host": "velocidade-fla.algartelecom.com.br:8080", "status": 1 }],
-    "testGlobals": { "ipAddress": "179.215.122.152", "ispName": "Claro", "ispId": 111794, "location": { "latitude": -24.2885, "longitude": -47.0256, "cityName": "Peruibe", "countryCode": "BR", "countryName": "Brazil", "regionCode": "SP", "regionName": "Sao Paulo" } },
-    "testSuiteGuid": false,
-    "engineOverrides": false,
-    "shaper": false,
-    "logging": { "jwtToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImQyYTAzOGZhLTZjODctNGE0NC1hNTc0LTM3NGY4NGFmNTgxNiIsInR5cGUiOiJoZWFkZXIifQ.DHbsSPjl4t4ujL3HsReHt4xaYZ23uioafVU6pGZl1ro", "buildId": 1646866193322, "level": "info" },
-    "engine": {}
-};
+// window.ST_PARAMS = {
+//     "subdomain": "algar",
+//     "testConfiguration": {
+//         "testConfigurationId": 788,
+//         "subscriptionId": 376,
+//         "subdomain": "algar",
+//         "createdDate": "2017-03-13T09:25:30.000Z",
+//         "modifiedDate": "2017-03-18T11:07:37.000Z",
+//         "allowedServers": 5,
+//         "serverNetwork": "customer",
+//         "dataProtectionContactId": 0,
+//         "resellerContactId": 0,
+//         "apiKey": null,
+//         "originHost": null,
+//         "autoStart": false,
+//         "clientIcon": "user",
+//         "customCssFile": "",
+//         "customerServers": [27262, 11520, 26486, 28443, 29662],
+//         "distanceUnits": "mi",
+//         "embedUrls": ["velocidade-fla.algartelecom.com.br", "velocidade-spo.algartelecom.com.br", "velocidade-lgs.algartelecom.com.br", "velocidade-ula.algartelecom.com.br", "velocidade-sdr.algartelecom.com.br", "velocidade.algartelecom.com.br"],
+//         "hostSelectDisabled": false,
+//         "httpsOnly": false,
+//         "isActive": true,
+//         "isUpgradeDisabled": false,
+//         "allowSpeedtestUrls": false,
+//         "logo": { "name": "rsz_1logo_algar.png", "cdnFileName": "788-1489860003519.png" },
+//         "passwordProtected": false,
+//         "themeColors": { "backgroundColor": "#ffffff", "primaryColor": "#0268a8" },
+//         "serverIcon": "internet",
+//         "speedLabel": "Mbps",
+//         "defaultMaxSpeed": "100",
+//         "testTypes": ["upload", "download", "jitter", "ping"],
+//         "ipServerAssignment": { "allowed": false, "enabled": false },
+//         "isPaid": true,
+//         "isEUCountry": false
+//     },
+//     "config": { "rootDomain": "speedtestcustom.com", "logoCdnUrl": "https:\u002F\u002Flogos.speedtestcustom.com\u002Fprod\u002F", "countriesWithVat": ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB"], "premiumSubscriptionBasePrice": 1995, "premiumSubscriptionServerPrice": 995, "auth0": { "domain": "ookla.auth0.com", "clientId": "qZ22rLQ3HmY102QN05DIQGqFkz3AHdFr", "loginRedirectUrl": "https:\u002F\u002Faccount.speedtestcustom.com", "logoutRedirectUrl": "https:\u002F\u002Faccount.speedtestcustom.com\u002Flogin", "forgotPasswordLink": "https:\u002F\u002Faccount.speedtestcustom.com\u002Freset-password", "authorization": { "audience": "https:\u002F\u002Fapi.st-connect.com" }, "management": { "connection": "OOKLA-AUTH" } }, "privacyPolicyUpdated": "2018-05-18" },
+//     "testReport": null,
+//     "serverList": [{ "url": "http:\u002F\u002Fvelocidade-spo.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -23.5, "lon": -46.6167, "name": "São Paulo", "cc": "BR", "distance": 60, "sponsor": "Algar Telecom", "id": "28443", "host": "velocidade-spo.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-lgs.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -27.8157, "lon": -50.3264, "name": "Lages", "cc": "BR", "distance": 319, "sponsor": "Algar Telecom", "id": "27262", "host": "velocidade-lgs.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-ula.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -18.9, "lon": -48.2833, "name": "Uberlândia", "cc": "BR", "distance": 381, "sponsor": "Algar Telecom", "id": "11520", "host": "velocidade-ula.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-sdr.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -12.9747, "lon": -38.4767, "name": "Salvador", "cc": "BR", "distance": 962, "sponsor": "Algar Telecom", "id": "26486", "host": "velocidade-sdr.algartelecom.com.br:8080", "status": 1 }, { "url": "http:\u002F\u002Fvelocidade-fla.algartelecom.com.br:8080\u002Fspeedtest\u002Fupload.php", "lat": -3.7183, "lon": -38.5428, "name": "Fortaleza", "cc": "BR", "distance": 1531, "sponsor": "Algar Telecom", "id": "29662", "host": "velocidade-fla.algartelecom.com.br:8080", "status": 1 }],
+//     "testGlobals": { "ipAddress": "179.215.122.152", "ispName": "Claro", "ispId": 111794, "location": { "latitude": -24.2885, "longitude": -47.0256, "cityName": "Peruibe", "countryCode": "BR", "countryName": "Brazil", "regionCode": "SP", "regionName": "Sao Paulo" } },
+//     "testSuiteGuid": false,
+//     "engineOverrides": false,
+//     "shaper": false,
+//     "logging": { "jwtToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImQyYTAzOGZhLTZjODctNGE0NC1hNTc0LTM3NGY4NGFmNTgxNiIsInR5cGUiOiJoZWFkZXIifQ.DHbsSPjl4t4ujL3HsReHt4xaYZ23uioafVU6pGZl1ro", "buildId": 1646866193322, "level": "info" },
+//     "engine": {}
+// };
+
+//Enviar email Form
+
+$(document).ready(function() {
+    var envia = document.getElementById("form_contato");
+    var send = $("#enviar");
+
+    $('#envia_form').submit(function() {
+        send.prop('disabled', true);
+        send.text('Enviando');
+        $('#resposta_email').html('');
+        var datastring = $(this).serialize();
+
+        $.ajax({
+            type: "POST",
+            url: "envia_email_form.php",
+            data: datastring,
+            dataType: "html",
+            success: function(data) {
+                if (data == 'OK') {
+                    var resp =
+                        '<div class="alert alert-success" role="alert">E-mail enviado com sucesso!</div>';
+                } else {
+                    var resp = '<div class="alert alert-danger" role="alert">' + data +
+                        '</div>';
+                }
+                $('#resposta_email').html(resp);
+                $('#envia_form').trigger("reset");
+                $("#Telefone").val("");
+                send.prop('disabled', false);
+                send.text('Enviar');
+
+            },
+            error: function() {
+                $('#resposta_email').html(
+                    '<div class="alert alert-danger" role="alert">Erro ao enviar e-mail</div>'
+                );
+            }
+        });
+        return false;
+    });
+});
