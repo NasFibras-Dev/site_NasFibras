c39461f3f3499c3db5a242ca0be69b5c631c98d4
NasFibras-Dev <101998063+NasFibras-Dev@users.noreply.github.com>
2022-03-24 16:08:59 -0300

    comment scripts

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,24 @@
-(function() {
-    const label = document.querySelector('label')
-    const check = document.querySelector('#check')
-    const nav = document.querySelector('header nav')
-    label.addEventListener('click', () => {
-        if (!(check.checked)) {
-            nav.classList.add('active')
-        } else {
-            nav.classList.remove('active')
-        }
-    })
-    console.log(label)
-})()
+// function() {
+//     const label = document.querySelector('label')
+//     const check = document.querySelector('#check')
+//     const nav = document.querySelector('header nav')
+//     label.addEventListener('click', () => {
+//         if (!(check.checked)) {
+//             nav.classList.add('active')
+//         } else {
+//             nav.classList.remove('active')
+//         }
+//     })
+//     console.log(label)
+// })()
+
+
+
+//script mopstra IP
+
+// let userip = document.querySelector = (".ip_selector");
+// const ipTela = document.createElement("p");
+
+// ipTela.appendChild("userip")
+// src = "https://l2.io/ip.js?var=userip"
+// document.innerText = "Seu IP é:", userip;
--- src/js/script.js
+++ src/js/script.js
@@ -49,42 +49,42 @@
 
 //Enviar email Form
 
-$(document).ready(function() {
-    var envia = document.getElementById("form_contato");
-    var send = $("#enviar");
+// $(document).ready(function() {
+//     var envia = document.getElementById("form_contato");
+//     var send = $("#enviar");
 
-    $('#envia_form').submit(function() {
-        send.prop('disabled', true);
-        send.text('Enviando');
-        $('#resposta_email').html('');
-        var datastring = $(this).serialize();
+//     $('#envia_form').submit(function() {
+//         send.prop('disabled', true);
+//         send.text('Enviando');
+//         $('#resposta_email').html('');
+//         var datastring = $(this).serialize();
 
-        $.ajax({
-            type: "POST",
-            url: "envia_email_form.php",
-            data: datastring,
-            dataType: "html",
-            success: function(data) {
-                if (data == 'OK') {
-                    var resp =
-                        '<div class="alert alert-success" role="alert">E-mail enviado com sucesso!</div>';
-                } else {
-                    var resp = '<div class="alert alert-danger" role="alert">' + data +
-                        '</div>';
-                }
-                $('#resposta_email').html(resp);
-                $('#envia_form').trigger("reset");
-                $("#Telefone").val("");
-                send.prop('disabled', false);
-                send.text('Enviar');
+//         $.ajax({
+//             type: "POST",
+//             url: "envia_email_form.php",
+//             data: datastring,
+//             dataType: "html",
+//             success: function(data) {
+//                 if (data == 'OK') {
+//                     var resp =
+//                         '<div class="alert alert-success" role="alert">E-mail enviado com sucesso!</div>';
+//                 } else {
+//                     var resp = '<div class="alert alert-danger" role="alert">' + data +
+//                         '</div>';
+//                 }
+//                 $('#resposta_email').html(resp);
+//                 $('#envia_form').trigger("reset");
+//                 $("#Telefone").val("");
+//                 send.prop('disabled', false);
+//                 send.text('Enviar');
 
-            },
-            error: function() {
-                $('#resposta_email').html(
-                    '<div class="alert alert-danger" role="alert">Erro ao enviar e-mail</div>'
-                );
-            }
-        });
-        return false;
-    });
-});
+//             },
+//             error: function() {
+//                 $('#resposta_email').html(
+//                     '<div class="alert alert-danger" role="alert">Erro ao enviar e-mail</div>'
+//                 );
+//             }
+//         });
+//         return false;
+//     });
+// });
